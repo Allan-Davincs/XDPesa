8b685c844ba98e1750981f9a3d00eae160e9c9a9
allan-davincs <davincstechproject@gmail.com>
2026-07-01 13:03:48 +0300

    Remove unnecessary comment from LoanController import statement

--- XDserver/src/main/java/com/codewithdavincs/xdpesa/controllers/LoanController.java
+++ XDserver/src/main/java/com/codewithdavincs/xdpesa/controllers/LoanController.java
@@ -5,7 +5,7 @@ import com.codewithdavincs.xdpesa.repositories.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List; // WEKA HUU!
+import java.util.List; 
 
 
 @RestController
